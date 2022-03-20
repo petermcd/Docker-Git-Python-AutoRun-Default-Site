be979598b62f131df0a486e1963358fde72ae07d
Peter McDonald <me@petermcdonald>
2022-03-20 22:46:37 +0000

    Disabling debug mode

--- Docker_Git_Python_AutoRun_Default_Site/settings.py
+++ Docker_Git_Python_AutoRun_Default_Site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0l!gd(#2lj8$6rt1zlq3xymf1j2^x^(8)1qknx8wu*c+q9pm+9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
